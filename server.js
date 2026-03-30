ffe61d54b38583cdc3e443b38d0858dfc9798682
root <root@ip-172-31-66-229.ec2.internal>
2026-03-30 17:37:14 +0000

    kjh

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello Git Practice Ap working finep 🚀");
+  res.send("Hello Git Practice Ap working finep working kr rha h  🚀");
 });
 
 app.get("/about", (req, res) => {
